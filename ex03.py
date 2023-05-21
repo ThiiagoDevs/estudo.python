1bf6ba1706137401055c5a8daa2a7ee19bf4cadd
Thiago Ribeiro <thiagohsantana@icloud.com>
2023-05-21 01:22:54 -0300

    estudopython

--- ex03.py
+++ ex03.py
@@ -13,20 +13,3 @@ Entry(root).place(x=120, y=40)
 Entry(root).place(x=120, y=70)
 
 root.mainloop()
-
-'''import random
-import string
-
-def gerar_senha(tamanho):
-    senha = ''
-    for _ in range(tamanho):
-        senha += random.choice(string.ascii_letters + string.digits)
-    return senha 
-    
-print('\n     Gerador de senhas ')
-print('>'*30)
-tamanho = int(input('Qual tamanho da sua senha: '))    
-print('>'*30)
-
-senha = gerar_senha(tamanho)
-print(f'Sua senha é {senha}')'''
